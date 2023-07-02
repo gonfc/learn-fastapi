98fe45abd1e6670b92d339d56f53600d9f5c88d5
Gonçalo Correia <gcorreia2001@gmail.com>
2023-07-02 12:31:35 +0100

    feat(api): add get method and respective tests

--- main.py
+++ main.py
@@ -37,7 +37,34 @@ def index() -> dict[str, dict[int, Item]]:
 
 
 @app.get('/items/{item_id}')
-def index(item_id: int) -> Item:
+def get_items_by_id(item_id: int) -> Item:
     if item_id not in items:
         raise HTTPException(status_code=404, detail=f"Item with id {item_id} was not found in the database.")
     return items[item_id]
+
+
+Selection = dict[str, str | int | float | Category | None]
+
+
+@app.get('/items/')
+def get_items_by_parameters(
+        name: str | None = None,
+        price: float | None = None,
+        count: int | None = None,
+        category: Category | None = None) \
+        -> dict[str, Selection | list[Item]]:
+    def check_item_validity(item: Item):
+        return all(
+            (
+                name is None or item.name == name,
+                price is None or item.price == price,
+                count is None or item.count != count,
+                category is None or item.category is category
+            )
+        )
+
+    selection = [item for item in items.values() if check_item_validity(item)]
+    return {
+        "query": {"name": name, "price": price, "count": count, "category": category},
+        "selection": selection
+    }
--- tests/test_server.py
+++ tests/test_server.py
@@ -10,3 +10,7 @@ SERVER_URL = os.getenv('SERVER_URL')
 
 assert requests.get(f"{SERVER_URL}").status_code == 200
 assert requests.get(f"{SERVER_URL}/items/1").status_code == 200
+
+assert requests.get("http://127.0.0.1:8000/items?name=plier").json().get(['selection'][0]) == [{'name': 'plier', 'price': 6.99, 'count': 70, 'id': 1, 'category': 'tools'}]
+assert requests.get("http://127.0.0.1:8000/items?price=6.99").json().get(['selection'][0]) != ''
+assert requests.get("http://127.0.0.1:8000/items?category=tools").json() != ''
